b5d7305f7120e6d630698bc12da0fc25059defd2
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2023-04-02 19:50:59 +0200

    Class Connexion & config
    il y a pas de fuite de mots de passe pas la peine de regarder

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+$config = array(
+    'administrateur' => array(
+        'host' => 'localhost',
+        'dbname' => 'MySportReservation',
+        'username' => 'administrateur',
+        'password' => 'mot_de_passe'
+    ),
+    'utilisateur' => array(
+        'host' => 'localhost',
+        'dbname' => 'MySportReservation',
+        'username' => 'utilisateur',
+        'password' => 'mot_de_passe'
+    ),
+    'lecteur' => array(
+        'host' => 'localhost',
+        'dbname' => 'MySportReservation',
+        'username' => 'lecteur',
+        'password' => 'mot_de_passe'
+    )
+);
+?>
