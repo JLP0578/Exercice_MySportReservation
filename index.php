aeb109eecde63bb07cd5298c448c5c568a3b0e2e
JLP0578 <j.lepivert@outlook.fr>
2023-04-18 12:21:14 +0200

    [Draft]: add test js function

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ require_once __DIR__.'/outils.php';
 </head>
 <body>
     <?php
-        include './views/login-signin.php';
+        // include './views/login-signin.php';
         $AccountTypeId = 3;
         require_once __DIR__.'/views/lists/list_activities.php';
 
@@ -73,5 +73,7 @@ require_once __DIR__.'/outils.php';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="./assets/scripts/outils.js"></script>
+    <script src="./assets/scripts/login-signin.js"></script>
 </body>
 </html>
---
+++ tests/scripts/login-signin.test.js
@@ -0,0 +1,19 @@
+/* ElementById */
+let login_email;
+let login_password;
+let login_csrf_token;
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.info("DOM fully loaded and parsed, start JS");
+
+
+    
+});
+
+function test_verifyUserName(){
+    let x = 5;
+    let y = 5;
+    console.assert(x + y == 11, "Expression returned 'false'");
+
+}
+
